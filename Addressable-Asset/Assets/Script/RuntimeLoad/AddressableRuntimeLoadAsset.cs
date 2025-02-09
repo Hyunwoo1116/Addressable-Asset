af4343faca2e5d9c46c1bd60a2d29817e22264b2
Leehyun-woo <lolkarma325@gmail.com>
2025-02-09 18:18:00 +0900

    feature {$Addressable} : GetDownloadSize
    Resource Download Size GetByteSize

--- Addressable-Asset/Assets/Script/RuntimeLoad/AddressableRuntimeLoadAsset.cs
+++ Addressable-Asset/Assets/Script/RuntimeLoad/AddressableRuntimeLoadAsset.cs
@@ -1,23 +1,31 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class AddressableRuntimeLoadAsset : MonoBehaviour
 {
-    public List<string> Resources = new List<string>()
-    {
-        "Audio", "Effects", "FBX", "Textures"
-    };
 
-    void Start()
+    async void Start()
     {
-        AsyncOperationHandle<long> handle = Addressables.GetDownloadSizeAsync("LargeMap");
-
         
     }
 
+    public async void DownloadAudioResource()
+    {
+        AsyncOperationHandle<long> handle = Addressables.GetDownloadSizeAsync("Assets/AssetStore/AS_Casual_Island_Game_Sounds_FREE/Jingles/Jingle_2.wav");
+
+        while (!handle.IsDone)
+        {
+            await Task.Yield();
+        }
+        if (handle.Result > 0 )
+        {
+            Debug.Log($"Download 받을것이 있음.{handle.Result}Bytes");
+        }
+    }
     // Update is called once per frame
     void Update()
     {
